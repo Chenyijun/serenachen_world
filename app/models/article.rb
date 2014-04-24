a40814856e94dfd01e8595be02db8d79dffa4e16
Annie Chen <annieche@andrew.cmu.edu>
2014-04-24 19:58:29 -0400

    created articles

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ActiveRecord::Base
+	belongs_to :categories
+
+	validates_presence_of :title, :content
+
+	scope :active, where('active = ?', true)
+  	scope :alphabetical, order('title')
+end
